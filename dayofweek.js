2975f17cc02dbc87c2c7ac7234b9480a6f37f112
audiothepoet <105013771+audiothepoet@users.noreply.github.com>
2022-11-17 17:25:41 -0600

    Update dayofweek.js

--- dayofweek.js
+++ dayofweek.js
@@ -47,7 +47,7 @@ if (day==0) {
         var img = document.createElement("img");
         img.src = "gifs/giphy-2.gif";
         img.height=200;
-        img.width=100;
+        img.width=200;
         // This next line will just add it to the <body> tag
         document.body.appendChild(img);
   } else if (day==5) {
